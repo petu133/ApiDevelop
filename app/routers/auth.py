cf171b28b152aae9a8c354e5ab447859652cfbef
petu133 <77856779+SmurfRegulus@users.noreply.github.com>
2022-04-11 14:05:27 -0300

    OAuth2 specification

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,15 +1,23 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from .. import schemas, models, utils, database, oauth2
 from sqlalchemy.orm import Session
-#from ..database import get_db
+
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     
-    #catch the data from the database that matches with the credential passed by the user
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first() 
+    #OAuth2PasswordRequestFrom stores de data as a dictionary. 
+    # {
+    #     "username" : " ",  Whatever value the user pass (email, pen name, id, etc)
+    #     "password" : " "
+    # }
+    # Now in Postman the credentials are no longer pass in through raw json on the body. Instead we pass it as form-data.
+
+    #Catch the data from the database that matches with the credential passed by the user
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first() 
     
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
