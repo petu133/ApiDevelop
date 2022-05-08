96f74763b8dacbfc9a1ed5017b00ea1e5a337b19
petu133 <77856779+SmurfRegulus@users.noreply.github.com>
2022-05-07 23:38:30 -0300

    CORS policy setted up

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI 
 #The fastapi library importation needs to be at the very top. This allows avoid some issues that could arise if not done in this way.
+from fastapi.middleware.cors import CORSMiddleware
 from random import randrange
 from .database import engine
 from . import models
@@ -9,12 +10,22 @@ from .config import Settings
 #--cmd commands-- venv\Scripts\activate.bat  "Enter to the environment"
 # uvicorn app.main:app --reload  "Reads app setting variable in main.py and executes the fastapi program"
 
-# models.Base.metadata.create_all(bind=engine) 
+#models.Base.metadata.create_all(bind=engine) 
 #Tells sql alchemy to run the create statement so that it generates the tables.
 #Since I executed all that logic from alembic, the line can be commnented. 
 
 
 app = FastAPI()
+
+#origins = ["https://www.google.com"] #Allows google to talk to our API, so not only the server domain can do it
+origins = ["*"] #Allows all sites to talk to our API
+app.add_middleware(
+    CORSMiddleware, #(In WebFrameworks) can think of a Middleware as some sort of function that runs before every request
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # When an http request occurs, include these routers, then the request go 
 # to the folder routers where all the path operation logic inhabits
