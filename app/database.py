2ce5f2b388b60d3da603a2fd7c042df0315e0a04
petu133 <77856779+SmurfRegulus@users.noreply.github.com>
2022-05-14 16:38:54 -0300

    issue connection

--- app/database.py
+++ app/database.py
@@ -16,7 +16,7 @@ SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.
 print(f"The database name is {settings.database_name}")
 print(type({settings.database_name}))
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, echo=True, echo_pool=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
