7812928906bfa2be5f21fedd98c0272a2930af82
petu133 <77856779+SmurfRegulus@users.noreply.github.com>
2022-05-14 15:30:17 -0300

    connect issue set ssl mode allow

--- app/database.py
+++ app/database.py
@@ -17,7 +17,7 @@ SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.
 print(f"The database name is {settings.database_name}")
 print(type({settings.database_name}))
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'sslmode': "allow"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
